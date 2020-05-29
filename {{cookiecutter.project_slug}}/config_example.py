7f38f00d6c36c996709be4b9444a3760e72cbb83
Guido Tournois <guido.tournois@vantage-ai.com>
2020-05-29 11:27:43 +0200

    Clean up code

--- {{cookiecutter.project_slug}}/config_example.py
+++
@@ -1,5 +0,0 @@
-from config import Config
-
-def print_param1():
-    config = Config()
-    print(config.param1)
--- {{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/main.py
@@ -1,6 +1,4 @@
-from config_example import print_param1
 from config import Config
-
 
 if __name__ == "__main__":
     c = Config(env='DEVELOPMENT')
