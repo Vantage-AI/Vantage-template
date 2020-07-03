ff0142ef0f6884c5a113b4f8a5debd7a307fff8e
Jasper Derikx <jasper.derikx@vantage-ai.com>
2020-07-03 11:58:39 +0200

    Debug

--- {{cookiecutter.project_slug}}/src/tests/{{cookiecutter.project_slug}}/test_logging.py
+++ {{cookiecutter.project_slug}}/src/tests/{{cookiecutter.project_slug}}/test_logging.py
@@ -4,17 +4,17 @@ import unittest
 class TestLogging(unittest.TestCase):
 
     def test_show_handler(self):
-        from logging import show_handler
+        from src.logging import show_handler
         show_handler()
 
     def test_show_formatter(self):
-        from logging import show_formatter
+        from src.logging import show_formatter
         self.assertEqual(show_formatter, 67)
 
     def test_logging_levels(self):
-        from logging import logging_levels
+        from src.logging import logging_levels
         self.assertEqual(logging_levels, 50)
 
     def test_log_exceptions(self):
-        from logging import log_exceptions
+        from src.logging import log_exceptions
         log_exceptions()
